dce9a4d6168382fbbef10316214e49d45d399f7a
Lauren <Lauren@104.39.75.172>
2017-08-31 12:49:48 -0400

    Added Score Class

---
+++ src/munchiespractice/Score.java
@@ -0,0 +1,16 @@
+
+package munchiespractice;
+
+/**
+ *
+ * @author Lauren
+ */
+public class Score {
+    int highScore;
+    int currentScore;
+    
+    public Score(){
+        this.highScore= highScore;
+        this.currentScore = currentScore;
+    }
+}
