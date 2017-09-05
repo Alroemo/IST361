495265cd8068d27b29a427aa7c3afffa9603c932
mlh5614 <mlh5614@psu.edu>
2017-09-05 13:20:46 -0400

    Update GridView.java
    added randomized grid, number of yello is number of primes, order is random

--- munchiespractice/src/munchiespractice/GridView.java
+++ munchiespractice/src/munchiespractice/GridView.java
@@ -50,20 +50,144 @@ public class GridView extends JFrame{
 //       mainPanel.add(lowerRight);
     }
     
-    public void CreateGrid(int row,int col){
+        public void CreateGrid(int row,int col){
+    int gridSize;  //max
         Container pane = getContentPane();
     pane.setLayout(new GridLayout(row, col));
-    for (int i = 0; i < row * col; i++) {
+    gridSize = row * col; //max
+    JButton buttons[];  //max
+    buttons = new JButton[50];
+    for (int i = 0; i < gridSize; i++) {
         //will be put controlled randomized numbers
-      JButton button = new JButton(Integer.toString(i + 1));
-      pane.add(button);
+        buttons[i] = new JButton(Integer.toString(i + 1));  //max
+        pane.add(buttons[i]);            //max
+      //  randomizeGrid(gridSize, buttons);
+      //JButton button = new JButton(Integer.toString(i + 1));  //original
+      //pane.add(button); //original
     }
-
+    randomizeGrid(gridSize, buttons);
     }
     
     
             
     
+public void randomizeGrid(int gridSize, JButton[] buttons)
+{
+	int numRight = 0;
+	int numWrong = 0;
+	int randomIndex = 0; //used later to determine random index for sorting grid
+	int maxDigit = 99; //change depending on grid size and max we want to use
+	
+	//Determine the number of correct answers (9-13 of 25, this part is depedent on # of jbuttons aka gridSize)
+	numRight = (int)(Math.random() * 5 + 9); //change this depending on the gridSize, hardcoded for 16 rn
+	numWrong = gridSize - numRight;
+	
+	//Set 
+	setPrimes(gridSize, maxDigit, buttons, numRight);     //primes are 0 -> numRight-1 indexes
+	setNonPrimes(gridSize, maxDigit, buttons, numRight, numWrong);  //non-primes are numright -> numright + numwrong
+	
+        
+	//Randomize order of the buttons
+	for (int i = 0; i < gridSize; i++)
+	{
+		randomIndex = (int)(Math.random() * 25); //hardcoded for now
+		//JButton a = buttons[i];
+		//buttons[i] = buttons[randomIndex];
+		//buttons[randomIndex] = a;
+                JButton a;
+                a = new JButton(buttons[i].getText());
+                buttons[i].setText(buttons[randomIndex].getText());
+                buttons[randomIndex].setText(a.getText());
+	}
+}
+
+public void setPrimes(int gridSize, int maxDigit, JButton buttons[], int numRight)
+{
+	//prime numbers are NOT divisible by any number greater than 1 other than itself
+	//int start = 2;
+	int primeNumber = 0;
+	boolean isPrime; //assume false until we get a prime
+	
+	for (int i = 0; i < numRight; i++)
+	{
+            isPrime = false;
+		while (isPrime == false)
+		{
+                    System.out.println(primeNumber);
+			primeNumber = (int) (Math.random() * 99 + 1); //pick numbers from 1 to maxDigit
+			
+			if (testPrime(primeNumber, maxDigit) == true)
+			{
+					isPrime = true;
+			}
+		}
+		
+                buttons[i].setText(Integer.toString(primeNumber));
+                buttons[i].setBackground(Color.yellow);
+		//buttons[i] = primeNumber; //need to get the prime from somewhere
+	}
+}
+
+public void setNonPrimes(int gridSize, int maxDigit, JButton buttons[], int numRight, int numWrong)
+{
+	//int start = 2;
+	int nonPrimeNumber = 0;
+	boolean isPrime; //assume true until we get a non-prime
+	
+	//non-prime numbers are divisible by any number greater than 1 other than itself
+	for (int i = numRight; i < gridSize; i++)
+	{
+            isPrime = true;
+		while (isPrime == true)
+		{
+			nonPrimeNumber = (int) (Math.random() * 99 + 1); //pick numbers from 1 to maxDigit
+			
+			if (testPrime(nonPrimeNumber, maxDigit) == false)
+			{
+					isPrime = false;
+			}
+		}
+		
+                buttons[i].setText(Integer.toString(nonPrimeNumber));
+	//	buttons[i] = nonPrimeNumber; //need to get the non-prime from somewhere
+	}
+}
+
+public boolean testPrime(int maybePrime, int maxDigit) //private? idk?
+{
+       // boolean result = true; //assume prime unless not
+		//code to check if a number is prime
+		
+                if (maybePrime < 2)
+                {
+                    return false;
+         //           result = false;
+                }
+    
+                if (maybePrime == 2)
+                {
+                    return true;
+           //         result = true;
+                }
+		//2 is the only even prime so check it first
+		if (maybePrime % 2 == 0)
+		{
+                    return false;
+		//	result = false;
+		}
+		
+		//check the odd primes next to save time/memory
+		for (int i = 3; i < maxDigit; i++)
+		{
+			if (maybePrime % i == 0 && maybePrime != i)
+			{
+                            return false;
+		//		result = false;
+			}
+		}
+		
+                return true;
+                
     
     
     
