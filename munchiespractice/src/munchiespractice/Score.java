d4a5537c679f6c4259731a988d4475260fc066ac
Lauren <Lauren@104.39.108.47>
2017-09-05 12:43:09 -0400

    Added Score Class

---
+++ munchiespractice/src/munchiespractice/Score.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package munchiespractice;
+
+/**
+ *
+ * @author Lauren
+ */
+public class Score {
+    int currentScore;
+    int highScore;
+    
+    public Score(){
+        currentScore = this.currentScore;
+        highScore = this.currentScore;
+    }
+}
