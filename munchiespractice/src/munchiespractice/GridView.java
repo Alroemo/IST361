84a7587cd6293024adc8937d3c2d3904baa0dded
Alroemo <avroeum@gmail.com>
2017-09-07 12:22:56 -0400

    Modified GridView to store the game score in a score variable instead of an int in Score.java

--- munchiespractice/src/munchiespractice/GridView.java
+++ munchiespractice/src/munchiespractice/GridView.java
@@ -1,15 +1,7 @@
 package munchiespractice;
 
-
-
-import java.awt.Color;
-
-import java.awt.Container;
-
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JButton;
 
@@ -29,525 +21,252 @@ import javax.swing.JLabel;
  * and open the template in the editor.
 
  */
-
-
-
 /**
-
  *
-
+ *
+ *
  * @author Lauren
-
+ *
  */
+public class GridView extends JFrame {
 
-public class GridView extends JFrame{
+    private int score = 0;
+    private Score currentScore = new Score(); 
+    JLabel scoreLabel = new JLabel("Score ");
 
-//    private JPanel mainPanel;
-
-//    private JButton topLeft;
-
-//    private JButton topMiddle;
-
-//    private JButton topRight;
-
-//    
-
-//    private JButton lowerLeft;
-
-//    private JButton lowerMiddle;
-
-//    private JButton lowerRight;
-private int score =0;
-JLabel scoreLabel = new JLabel("Score ");
-    
-
-    public GridView(){
+    public GridView() {
 
         this.score = score;
-        
-
-        
 
         setSize(700, 500);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-//       mainPanel = new JPanel (new GridLayout (2,3));
-
-//       add(mainPanel);
-
-//       topLeft = new JButton("1");
-
-//       topMiddle = new JButton("2");
-
-//       topRight = new JButton("3");
-
-//       lowerLeft = new JButton("4");
-
-//       lowerMiddle = new JButton("5");
-
-//       lowerRight = new JButton("6");
-
-//      
-
-//       
-
-//       mainPanel.add(topLeft);
-
-//       mainPanel.add(topMiddle);
-
-//       mainPanel.add(topRight);
-
-//       mainPanel.add(lowerLeft);
-
-//       mainPanel.add(lowerMiddle);
-
-//       mainPanel.add(lowerRight);
-
     }
 
-    
+    public void CreateGrid(int row, int col) {
 
-        public void CreateGrid(int row,int col){
-          
-
-    int gridSize;  //max
+        int gridSize;  //max
 
         Container pane = getContentPane();
 
-    pane.setLayout(new GridLayout(row, col));
+        pane.setLayout(new GridLayout(row, col));
 
-    gridSize = row * col; //max
+        gridSize = row * col; //max
 
-    JButton buttons[];  //max
+        JButton buttons[];  //max
 
-    buttons = new JButton[50];
+        buttons = new JButton[50];
 
-    for (int i = 0; i < gridSize; i++) {
+        for (int i = 0; i < gridSize; i++) {
 
-        //will be put controlled randomized numbers
+            //will be put controlled randomized numbers
+            buttons[i] = new JButton(Integer.toString(i + 1));  //max
 
-        buttons[i] = new JButton(Integer.toString(i + 1));  //max
-        
+            pane.add(buttons[i]);
 
-        pane.add(buttons[i]);    
-        
-        //added
-        //lauren added button action listener
-          pane.add(scoreLabel);
-        
-        buttons[i].addActionListener(new ActionListener() {;//max
+            //added
+            //lauren added button action listener
+            pane.add(scoreLabel);
+
+            buttons[i].addActionListener(new ActionListener() {
+                ;//max
 
       
         @Override
-        public void actionPerformed(ActionEvent event){
-        JButton tempbutton = (JButton) event.getSource();
-        String test=tempbutton.getText();
-       int numbertest=Integer.parseInt(test);
-                if(testPrime(numbertest, 100)==true){
-                    
-                    System.out.println(numbertest +"prime");
-                    score = score+100;
-                    scoreLabel.setText("Score: "+ score);
-                }else
-                {
-                    System.out.println(numbertest+"not prime");
-                    score= score-100;
-                    scoreLabel.setText("Score: "+ score);
-                }
-                    
-                    
-                    
+                public void actionPerformed(ActionEvent event) {
+                    JButton tempbutton = (JButton) event.getSource();
+                    String test = tempbutton.getText();
+                    int numbertest = Integer.parseInt(test);
+                    if (testPrime(numbertest, 100) == true) {
+
+                        System.out.println(numbertest + "prime");
+                        currentScore.setScore(currentScore.getCurrentScore() + 100);
+                        scoreLabel.setText("Score: " + currentScore.getCurrentScore());
+                    } else {
+                        System.out.println(numbertest + "not prime");
+                        currentScore.setScore(currentScore.getCurrentScore() - 100);
+                        scoreLabel.setText("Score: " + currentScore.getCurrentScore());
+                    }
                     ;
-                
-                
-                
-                
-        randomizeGrid(gridSize,buttons);
-        
-        }}
-        //stop of laurens edit
-                
-                
-                
-                
-      //  randomizeGrid(gridSize, buttons);
 
-      //JButton button = new JButton(Integer.toString(i + 1));  //original
+                    randomizeGrid(gridSize, buttons);
 
-      //pane.add(button); //original
+                }
+            }
+            //stop of laurens edit
 
-        );
+            //  randomizeGrid(gridSize, buttons);
+
+            //JButton button = new JButton(Integer.toString(i + 1));  //original
+
+            //pane.add(button); //original
+
+            );
         };
 
-    randomizeGrid(gridSize, buttons);
+        randomizeGrid(gridSize, buttons);
 
     }
 
-       
-    
+    public void randomizeGrid(int gridSize, JButton[] buttons) {
 
-    
+        int numRight = 0;
 
-            
+        int numWrong = 0;
 
-    
+        int randomIndex = 0; //used later to determine random index for sorting grid
 
-public void randomizeGrid(int gridSize, JButton[] buttons)
+        int maxDigit = 99; //change depending on grid size and max we want to use
 
-{
+        //Determine the number of correct answers (9-13 of 25, this part is depedent on # of jbuttons aka gridSize)
+        numRight = (int) (Math.random() * 5 + 9); //change this depending on the gridSize, hardcoded for 16 rn
 
-	int numRight = 0;
+        numWrong = gridSize - numRight;
 
-	int numWrong = 0;
+        //Set 
+        setPrimes(gridSize, maxDigit, buttons, numRight);     //primes are 0 -> numRight-1 indexes
 
-	int randomIndex = 0; //used later to determine random index for sorting grid
+        setNonPrimes(gridSize, maxDigit, buttons, numRight, numWrong);  //non-primes are numright -> numright + numwrong
 
-	int maxDigit = 99; //change depending on grid size and max we want to use
+        //Randomize order of the buttons
+        for (int i = 0; i < gridSize; i++) {
 
-	
+            randomIndex = (int) (Math.random() * gridSize); //hardcoded for now
 
-	//Determine the number of correct answers (9-13 of 25, this part is depedent on # of jbuttons aka gridSize)
+            //JButton a = buttons[i];
+            //buttons[i] = buttons[randomIndex];
+            //buttons[randomIndex] = a;
+            JButton a;
 
-	numRight = (int)(Math.random() * 5 + 9); //change this depending on the gridSize, hardcoded for 16 rn
+            a = new JButton(buttons[i].getText());
 
-	numWrong = gridSize - numRight;
+            buttons[i].setText(buttons[randomIndex].getText());
 
-	
+            buttons[randomIndex].setText(a.getText());
 
-	//Set 
+        }
 
-	setPrimes(gridSize, maxDigit, buttons, numRight);     //primes are 0 -> numRight-1 indexes
+        highlightAnswers(buttons, gridSize, maxDigit); //show the answers
 
-	setNonPrimes(gridSize, maxDigit, buttons, numRight, numWrong);  //non-primes are numright -> numright + numwrong
+    }
 
-	
+    public void setPrimes(int gridSize, int maxDigit, JButton buttons[], int numRight) {
 
-        
+        //prime numbers are NOT divisible by any number greater than 1 other than itself
+        //int start = 2;
+        int primeNumber = 0;
 
-	//Randomize order of the buttons
+        boolean isPrime; //assume false until we get a prime
 
-	for (int i = 0; i < gridSize; i++)
-
-	{
-
-		randomIndex = (int)(Math.random() * gridSize); //hardcoded for now
-
-		//JButton a = buttons[i];
-
-		//buttons[i] = buttons[randomIndex];
-
-		//buttons[randomIndex] = a;
-
-                JButton a;
-
-                a = new JButton(buttons[i].getText());
-
-                buttons[i].setText(buttons[randomIndex].getText());
-
-                buttons[randomIndex].setText(a.getText());
-
-	}
-
-	highlightAnswers(buttons, gridSize, maxDigit); //show the answers
-
-}
-
-
-
-public void setPrimes(int gridSize, int maxDigit, JButton buttons[], int numRight)
-
-{
-
-	//prime numbers are NOT divisible by any number greater than 1 other than itself
-
-	//int start = 2;
-
-	int primeNumber = 0;
-
-	boolean isPrime; //assume false until we get a prime
-
-	
-
-	for (int i = 0; i < numRight; i++)
-
-	{
+        for (int i = 0; i < numRight; i++) {
 
             isPrime = false;
 
-		while (isPrime == false)
+            while (isPrime == false) {
 
-		{
+                System.out.println(primeNumber);
 
-                    System.out.println(primeNumber);
+                primeNumber = (int) (Math.random() * maxDigit + 1); //pick numbers from 1 to maxDigit
 
-			primeNumber = (int) (Math.random() * maxDigit + 1); //pick numbers from 1 to maxDigit
+                if (testPrime(primeNumber, maxDigit) == true) {
 
-			
+                    isPrime = true;
 
-			if (testPrime(primeNumber, maxDigit) == true)
+                }
 
-			{
+            }
 
-					isPrime = true;
+            buttons[i].setText(Integer.toString(primeNumber));
 
-			}
+            buttons[i].setBackground(Color.yellow);
 
-		}
+            //buttons[i] = primeNumber; //need to get the prime from somewhere
+        }
 
-		
+    }
 
-                buttons[i].setText(Integer.toString(primeNumber));
+    public void setNonPrimes(int gridSize, int maxDigit, JButton buttons[], int numRight, int numWrong) {
 
-                buttons[i].setBackground(Color.yellow);
+        //int start = 2;
+        int nonPrimeNumber = 0;
 
-		//buttons[i] = primeNumber; //need to get the prime from somewhere
+        boolean isPrime; //assume true until we get a non-prime
 
-	}
-
-}
-
-
-
-public void setNonPrimes(int gridSize, int maxDigit, JButton buttons[], int numRight, int numWrong)
-
-{
-
-	//int start = 2;
-
-	int nonPrimeNumber = 0;
-
-	boolean isPrime; //assume true until we get a non-prime
-
-	
-
-	//non-prime numbers are divisible by any number greater than 1 other than itself
-
-	for (int i = numRight; i < gridSize; i++)
-
-	{
+        //non-prime numbers are divisible by any number greater than 1 other than itself
+        for (int i = numRight; i < gridSize; i++) {
 
             isPrime = true;
 
-		while (isPrime == true)
+            while (isPrime == true) {
 
-		{
+                nonPrimeNumber = (int) (Math.random() * maxDigit + 1); //pick numbers from 1 to maxDigit
 
-			nonPrimeNumber = (int) (Math.random() * maxDigit + 1); //pick numbers from 1 to maxDigit
+                if (testPrime(nonPrimeNumber, maxDigit) == false) {
 
-			
-
-			if (testPrime(nonPrimeNumber, maxDigit) == false)
-
-			{
-
-					isPrime = false;
-
-			}
-
-		}
-
-		
-
-                buttons[i].setText(Integer.toString(nonPrimeNumber));
-
-	//	buttons[i] = nonPrimeNumber; //need to get the non-prime from somewhere
-
-	}
-
-}
-
-
-
-public boolean testPrime(int maybePrime, int maxDigit) //private? idk?
-
-{
-
-       // boolean result = true; //assume prime unless not
-
-		//code to check if a number is prime
-
-		
-
-                if (maybePrime < 2)
-
-                {
-
-                    return false;
-
-         //           result = false;
+                    isPrime = false;
 
                 }
 
-    
+            }
 
-                if (maybePrime == 2)
+            buttons[i].setText(Integer.toString(nonPrimeNumber));
 
-                {
-
-                    return true;
-
-           //         result = true;
-
-                }
-
-		//2 is the only even prime so check it first
-
-		if (maybePrime % 2 == 0)
-
-		{
-
-                    return false;
-
-		//	result = false;
-
-		}
-
-		
-
-		//check the odd primes next to save time/memory
-
-		for (int i = 3; i < maxDigit; i++)
-
-		{
-
-			if (maybePrime % i == 0 && maybePrime != i)
-
-			{
-
-                            return false;
-
-		//		result = false;
-
-			}
-
-		}
-
-		
-
-                return true;
-
-                
-
-}    
-
-    
-public void highlightAnswers(JButton buttons[], int gridSize, int maxDigit)
-{
-    for (int i = 0; i < gridSize; i++)
-    {
-        if (testPrime(Integer.parseInt(buttons[i].getText()), maxDigit) == true)
-        {
-             buttons[i].setBackground(Color.yellow);
+            //	buttons[i] = nonPrimeNumber; //need to get the non-prime from somewhere
         }
-   
+
     }
-}
-    
 
+    public boolean testPrime(int maybePrime, int maxDigit) //private? idk?
+    {
 
+        // boolean result = true; //assume prime unless not
+        //code to check if a number is prime
+        if (maybePrime < 2) {
 
+            return false;
 
-//    public void addTopMiddleListener(ActionListener al){
+            //           result = false;
+        }
 
-//        topMiddle.addActionListener(al); 
+        if (maybePrime == 2) {
 
-//    
+            return true;
 
-//    }
+            //         result = true;
+        }
 
-//    public void addTopRightListener(ActionListener al){
+        //2 is the only even prime so check it first
+        if (maybePrime % 2 == 0) {
 
-//        topRight.addActionListener(al); 
+            return false;
 
-//    }
+            //	result = false;
+        }
 
-//public void addlowerLeftListener(ActionListener al){
+        //check the odd primes next to save time/memory
+        for (int i = 3; i < maxDigit; i++) {
 
-//        lowerLeft.addActionListener(al); 
+            if (maybePrime % i == 0 && maybePrime != i) {
 
-//    }
+                return false;
 
-//public void addlowerMiddleListener(ActionListener al){
+                //		result = false;
+            }
 
-//        lowerMiddle.addActionListener(al); 
+        }
 
-//    }
+        return true;
 
-//public void addlowerRightListener(ActionListener al){
+    }
 
-//        lowerRight.addActionListener(al); 
+    public void highlightAnswers(JButton buttons[], int gridSize, int maxDigit) {
+        for (int i = 0; i < gridSize; i++) {
+            if (testPrime(Integer.parseInt(buttons[i].getText()), maxDigit) == true) {
+                buttons[i].setBackground(Color.yellow);
+            }
 
-//    }
-
-//
-
-//public void changeNumberforTopLeftButton(){
-
-//    int b = (int)(Math.random()* 50 +1);
-
-//    
-
-//    topLeft.setText(b+ "");
-
-//}
-
-//public void changeNumberforTopMiddleButton(){
-
-//    int b = (int)(Math.random()* 50 +1);
-
-//    
-
-//    topMiddle.setText(b+ "");
-
-//}
-
-//public void changeNumberforTopRightButton(){
-
-//    int b = (int)(Math.random()* 50 +1);
-
-//    
-
-//    topRight.setText(b+ "");
-
-//}
-
-//
-
-//public void changeNumberforlowerLeftButton(){
-
-//    int b = (int)(Math.random()* 50 +1);
-
-//    
-
-//    lowerLeft.setText(b+ "");
-
-//}
-
-//public void changeNumberforlowerMiddleButton(){
-
-//    int b = (int)(Math.random()* 50 +1);
-
-//    
-
-//    lowerMiddle.setText(b+ "");
-
-//}
-
-//public void changeNumberforlowerRightButton(){
-
-//    int b = (int)(Math.random()* 50 +1);
-
-//    
-
-//    lowerRight.setText(b+ "");
-
-//}
-
-
-
-
-
-
+        }
+    }
 
 }
