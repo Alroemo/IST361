3470d492451479d78321d3eb916f78fae0357589
bxe5056 <bxe5056@WGATEE206-120.win.psu.edu>
2017-09-21 12:23:22 -0400

    Created Tower class

---
+++ TowerDefense/src/towerdefense/Tower.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package towerdefense;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author bxe5056
+ */
+public class Tower {
+    
+    private String name;
+    private int type;
+    private int damage; 
+    private int radius;
+    private int cost;
+    private Image img;
+    
+    public Tower(){
+        
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the damage
+     */
+    public int getDamage() {
+        return damage;
+    }
+
+    /**
+     * @param damage the damage to set
+     */
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * @return the cost
+     */
+    public int getCost() {
+        return cost;
+    }
+
+    /**
+     * @param cost the cost to set
+     */
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    /**
+     * @return the img
+     */
+    public Image getImage() {
+        return img;
+    }
+
+    /**
+     * @param img the img to set
+     */
+    public void setImage(Image img) {
+        this.img = img;
+    }
+}
