965cbb5e5adba2620974954e85010b9bbc23dcc0
Wesley Mauk <WesleyM77@users.noreply.github.com>
2017-10-03 12:38:58 -0400

    Added Wave
    Pass wave the current level and a randomly selected wavetype modifier (0 through 6). Wavetype modifier 7 is reserved for the boss wave (waves of multiples of 5).

---
+++ TowerDefense/src/towerdefense/Wave.java
@@ -0,0 +1,104 @@
+package towerdefense;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author wbm5061
+ */
+public class Wave {
+    private int level;
+    private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+       
+    
+    /**
+     * 
+     * @param lvl 
+     * @param waveType 0-4 (see enemy/wave design doc)
+     */
+    public Wave(int lvl, int waveType)
+    {
+        level = lvl;
+        int numberOfEnemies = 19 + level;
+        if(numberOfEnemies % 2 == 1)
+            numberOfEnemies++;
+        // Initialize the enemylist based on the waveType
+        switch(waveType)
+        {
+            case 0:
+                for(int i = 0; i < numberOfEnemies/2; i++)
+                {
+                    enemies.add(new Enemy(1, level));
+                    enemies.add(new Enemy(3, level));
+                }
+                break;
+            case 1:
+                for(int i = 0; i < numberOfEnemies; i++)
+                {
+                    enemies.add(new Enemy(2, level));
+                }
+                break;
+            case 2:
+                for(int i = 0; i < numberOfEnemies/4; i++)
+                {
+                    enemies.add(new Enemy(1, level));
+                    enemies.add(new Enemy(3, level));
+                    enemies.add(new Enemy(0, level));
+                    enemies.add(new Enemy(2, level));
+                }
+                break;
+            case 3:
+                for(int i = 0; i < numberOfEnemies; i++)
+                {
+                    enemies.add(new Enemy(3, level));
+                }
+                break;
+            case 4:
+                for(int i = 0; i < numberOfEnemies; i++)
+                {
+                    enemies.add(new Enemy(1, level));
+                }
+                break;
+            case 5:
+                for(int i = 0; i < numberOfEnemies; i++)
+                {
+                    enemies.add(new Enemy(0, level));
+                }
+                break;
+            case 6:
+                for(int i = 0; i < numberOfEnemies/4; i++)
+                {
+                    enemies.add(new Enemy(1, level));
+                    enemies.add(new Enemy(3, level));
+                    enemies.add(new Enemy(1, level));
+                    enemies.add(new Enemy(2, level));
+                }
+                break;
+            case 7:
+                enemies.add(new Enemy(4,level));
+                numberOfEnemies = Math.floorDiv(numberOfEnemies, 4);
+                for (int i = 0; i < numberOfEnemies; i++) {
+                    enemies.add(new Enemy(2, level));                       
+                }
+                break;
+            default:
+                System.out.println("Error in passing waveType to a Wave class");
+                break;
+        }
+        
+       
+    }
+
+    /**
+     * @return the enemies
+     */
+    public ArrayList<Enemy> getEnemies() {
+        return enemies;
+    }
+    
+    public void killEnemy(int index)
+    {
+        enemies.remove(index);
+    }
+    
+}
