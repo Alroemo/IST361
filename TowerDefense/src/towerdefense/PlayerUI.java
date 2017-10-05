84644a7dfa17cdf9bb1d2191c8c138070099488e
Lauren <Lauren@104.39.239.143>
2017-10-05 12:41:23 -0400

    added nextWave Method addition in PlayerUI

--- TowerDefense/src/towerdefense/PlayerUI.java
+++ TowerDefense/src/towerdefense/PlayerUI.java
@@ -280,7 +280,7 @@ public class PlayerUI extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        thePlayerController.setStage();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     public void populateUI()
